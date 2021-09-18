3d7b23b53e0ef1fbb7929a439bbbb97beb3b0e4e
Barry Gitarts <bgitarts@gmail.com>
2021-09-17 20:56:59 -0400

    add subgrid to info component

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,15 +1,24 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { styled } from '@mui/system';
 
-//@ts-ignore
 const Container = styled('div')(({ theme }) => ({
+  display: 'grid',
   background: 'black',
   gridColumn: '6 / 12',
-  gridRow: 'a / l'
+  gridRow: 'a / l',
+  gridTemplateColumns: ['a6','a7', 'a8', 'a9', 'a10', 'a11'].map(x => `[${x}] 1fr `).join(''),
+  gridTemplateRows: 'abcdefghijk'.split('').map(x => `[${x}] 1fr `).join(''),
 }));
+
+const TestSpan = styled('span')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'flex-end',
+  gridColumnStart: 'a7',
+  color: 'white'
+}))
 
 export default function () {
   return (
-    <Container>test</Container>
+    <Container><TestSpan>test</TestSpan></Container>
   );
 }
